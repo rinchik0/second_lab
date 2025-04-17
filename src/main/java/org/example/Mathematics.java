2e067b8ecd5b1505e14582b0c8e60fa1d5593b74
rinchik0 <ariadna.kolupaeva@yandex.ru>
2025-04-17 04:28:40 +0300

    Добавлены методы проверки корректности выражения, тестирование этих методов

--- src/main/java/org/example/Mathematics.java
+++ src/main/java/org/example/Mathematics.java
@@ -1,15 +1,58 @@
 package org.example;
 
+import java.util.*;
+
 public class Mathematics {
     private static Mathematics singleton;
-    private String arithmeticString;
+    private String expression;
+    private boolean hasString, isCorrect;
+    private static final Set<String> functions = Set.of("sin", "cos", "tan", "sqrt", "log", "abs");
     private Mathematics() {}
     public static Mathematics getInstance() {
-        if (singleton == null)
+        if (singleton == null) {
             singleton = new Mathematics();
+            singleton.hasString = false;
+        }
         return singleton;
     }
-    public void setArithmeticString(String string) {
-        arithmeticString = string;
+    public void setExpression(String string) {
+        expression = string;
+        hasString = true;
     }
+    public String getExpression() {
+        return expression;
+    }
+    public boolean isBracketsCorrect() {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(')
+                stack.push(expression.charAt(i));
+            if (expression.charAt(i) == ')') {
+                if (stack.empty())
+                    return false;
+                else stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+    public boolean isSyntaxCorrect() {
+        return !expression.matches(".*[^0-9+\\-*/^()a-zA-Z].*");
+    }
+    public boolean isExpressionCorrect() {
+        if (hasString)
+            return isSyntaxCorrect() && isBracketsCorrect();
+        return false;
+    }
+//    public String conversionToPostfixNotation() {
+//        Stack<Character> operations = new Stack<Character>();
+//        List<String> numbers = new ArrayList<String>();
+//        int number = 0;
+//        for (int i = 0; i < expression.length(); i++) {
+//            if (Character.isDigit(expression.charAt(i)))
+//                number = number * 10 +
+//        }
+//    }
+//    public double calculate() {
+//
+//    }
 }
---
+++ src/test/java/org/example/MathematicsTest.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathematicsTest {
+    @Test
+    void setGetExpressionFirst() {
+        Mathematics math = Mathematics.getInstance();
+
+        String expected = "1 + 2";
+
+        math.setExpression(expected);
+
+        String actual = math.getExpression();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setGetExpressionSecond() {
+        Mathematics math = Mathematics.getInstance();
+
+        String expected = "1 + 2";
+
+        math.setExpression(expected);
+
+        expected += " + 3";
+
+        String actual = math.getExpression();
+
+        Assertions.assertNotEquals(expected, actual);
+    }
+    @Test
+    void isBracketsCorrectFirst() {
+        Mathematics math = Mathematics.getInstance();
+        math.setExpression("(1+2)-1+2+3*(5+(6+8*(7+10)))");
+
+        boolean actual = math.isBracketsCorrect();
+
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void isBracketsCorrectSecond() {
+        Mathematics math = Mathematics.getInstance();
+        math.setExpression("(1+2)-1+2+3*(5+(6+8*(7+10))");
+
+        boolean actual = math.isBracketsCorrect();
+
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void isSyntaxCorrectFirst() {
+        Mathematics math = Mathematics.getInstance();
+        math.setExpression("4#5");
+
+        boolean actual = math.isSyntaxCorrect();
+
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void isSyntaxCorrectSecond() {
+        Mathematics math = Mathematics.getInstance();
+        math.setExpression("(1/2)-1+2log12+3z*(5y+(6+8*(7sin(x)+10))");
+
+        boolean actual = math.isSyntaxCorrect();
+
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
